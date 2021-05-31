12601b176cb97430e75449d85be3df7e054ec11d
Duck <swatduck@gmail.com>
2021-05-31 15:39:44 +0800

    新增_專案上傳

---
+++ ParserCureencyRate/Model.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace ParserCureencyRate
+{
+    class Model
+    {
+    }
+
+    public class CurrencyRateModel
+    {
+        public string msg { get; set; }
+
+        public string status { get; set; }
+
+        public string guest { get; set; }
+
+        public IEnumerable<CurrencyData> data { get; set; }
+    }
+
+
+    public class CurrencyData
+    {
+        public DateTime? UP_DATE { get; set; }
+
+        public string CRRN_CD { get; set; }
+
+        public string UP_PERSON { get; set; }
+
+        public decimal IN_RATE { get; set; }
+
+        public int TEN_DAY { get; set; } = 0;
+
+        public decimal EX_RATE { get; set; }
+
+        public string MON { get; set; }
+
+        public int? ORDER_FLAG { get; set; } = 0;
+
+        public string YEAR { get; set; }
+    }
+
+
+    [Table("CO_CurrencyRate")]
+    public class CO_CurrencyRate
+    {
+        public string CurrencyCode { get; set; }
+
+        public DateTime CurrencyDate { get; set; }
+
+        public decimal CurrencyRate { get; set; }
+    }
+}
---
+++ ParserCureencyRate/Program.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Dapper;
+using System.Configuration;
+using Dapper.Contrib.Extensions;
+using System.Threading;
+
+namespace ParserCureencyRate
+{
+    class Program
+    {
+        protected static readonly string InternalContext = ConfigurationManager.ConnectionStrings["NKMT"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+            for (int y = 2019; y <= 2021; y++)
+            {
+                for (int m = 1; m < 13; m++)
+                {
+                    Console.WriteLine(string.Format("執行{0}年{1}月匯率", y,m));
+
+
+                    //線上匯率API
+                    string url = @"https://portal.sw.nat.gov.tw/APGQO/GC331!query";
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    NameValueCollection postParams = HttpUtility.ParseQueryString(string.Empty);
+                    //postParams.Add("formBean.crrnCd", _CurrencyCode);
+                    postParams.Add("formBean.year", y.ToString());
+                    postParams.Add("formBean.mon", m.ToString().PadLeft(2, '0'));
+                    //postParams.Add("formBean.tenDay", d.ToString());
+
+
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postParams.ToString());
+                    using (Stream reqStream = request.GetRequestStream())
+                    {
+                        reqStream.Write(byteArray, 0, byteArray.Length);
+                    }
+
+                    string responseStr = string.Empty;
+                    //發出Request
+                    using (WebResponse response = request.GetResponse())
+                    {
+                        using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                        {
+                            responseStr = sr.ReadToEnd();
+                        }
+                    }
+
+                    var data = JsonConvert.DeserializeObject<CurrencyRateModel>(responseStr);
+
+
+                    List<CO_CurrencyRate> currencyRateList = new List<CO_CurrencyRate>();
+
+                    var tempDate = new DateTime(y, m, 1);
+
+                    for (int d = 1; d <= tempDate.AddMonths(1).AddDays(-1).Day; d++)
+                    {
+                        int tenDay = 0;
+
+                        if(d<11)
+                        {
+                            tenDay = 1;
+                        }
+                        else if(d >= 11 && d < 21)
+                        {
+                            tenDay = 2;
+                        }
+                        else if (d >= 21)
+                        {
+                            tenDay = 2;
+                        }
+
+
+                        currencyRateList.Add(new CO_CurrencyRate()
+                        {
+                            CurrencyCode = "RMB",
+                            CurrencyRate = data.data.FirstOrDefault(x => x.CRRN_CD == "CNY" && x.TEN_DAY == tenDay).IN_RATE,
+                            CurrencyDate = new DateTime(y, m, d)
+                        });
+
+                        currencyRateList.Add(new CO_CurrencyRate()
+                        {
+                            CurrencyCode = "USD",
+                            CurrencyRate = data.data.FirstOrDefault(x => x.CRRN_CD == "USD" && x.TEN_DAY == tenDay).IN_RATE,
+                            CurrencyDate = new DateTime(y, m, d)
+                        });
+
+                        currencyRateList.Add(new CO_CurrencyRate()
+                        {
+                            CurrencyCode = "EUR",
+                            CurrencyRate = data.data.FirstOrDefault(x => x.CRRN_CD == "EUR" && x.TEN_DAY == tenDay).IN_RATE,
+                            CurrencyDate = new DateTime(y, m, d)
+                        });
+                    }
+
+
+                    using (var cn = new SqlConnection(InternalContext))
+                    {
+                        cn.Insert(currencyRateList); ;
+                    }
+
+
+                    Console.WriteLine("寫入完成");
+                    Console.WriteLine("等待五秒...");
+
+                    Thread.Sleep(5000);
+                }
+            }
+
+
+
+
+        }
+    }
+}
